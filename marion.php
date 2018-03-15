af3c124798941ffbcfb6b7beacbae7f998158582
mbasprof <0rn1t0Github>
2018-03-15 09:43:04 -0400

    Ajout du fichier marion.php avec hyperliens dans nav principale

---
+++ marion.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="">
+        <script></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <nav id="main-nav">
+                    <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="marion.php">Marion</a></li>
+                        <li><a href="test.php">Mohammed</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>Page Marion</h1>
+            </main>
+            <footer>
+            </footer>
+        </div>
+    </body>
+</html>
